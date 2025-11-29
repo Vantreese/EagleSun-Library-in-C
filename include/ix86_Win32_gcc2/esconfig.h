e4430b924704b44303c9d959e023221320536488
Vantreese <70675231+Vantreese@users.noreply.github.com>
2025-11-29 08:28:21 -0500

    Add esconfig.h for EagleSun Library configuration

---
+++ include/ix86_Win32_gcc2/esconfig.h
@@ -0,0 +1,38 @@
+/************************************************************************
+                            EagleSun Library
+             (c) 2000, 2001, 2002, 2014 Terry Ron Vantreese
+************************************************************************/
+/************************************************************************
+Name:	esconfig.h
+Date:	7 July 2000
+Update:	27 July 2000
+Update: 25 June 2001
+Update: 23 August 2001
+Update: 22 February 2002
+Update:	2 October 2014
+
+The purpose of esconfig.h is to minimize the number of modifications of
+the source code when porting across different systems.
+
+Define name convention used follows the pattern
+<Hardware>_<OperatingSystem>_<Compiler>
+and should be used to decide which code to compile, or crash if a
+platform was not properly determined.
+************************************************************************/
+
+#ifndef _EagleSun_Library_Configuration_
+#define _EagleSun_Library_Configuration_
+
+/*
+#define axp_ovms_decc
+#define ix86_MSWin_msvc
+#define ix86_Unix_gcc2
+#define ix86_Win32_gcc2
+#define sparc_Solaris_spro
+#define sparc_Unix_gcc2
+#define vax_ovms_decc
+*/
+
+#define ix86_Win32_gcc2
+
+#endif
